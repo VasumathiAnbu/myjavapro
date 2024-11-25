b457049ac2fa6a4def0d59a6b63bf8b2d152389c
VasumathiAnbu <vasumathianbu18@gmail.com>
2024-11-25 15:39:09 +0530

    praveena

--- src/com/botree/Main.java
+++ src/com/botree/Main.java
@@ -4,12 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Git Repo");
-
-		System.out.println("Hello master");
-
-
-		System.out.println("Hello mybranch");
+		System.out.println("Hi praveena");
+		
 	}
 
 }
